fbe969169844a727a25fa9f3aba3448c087107d7
RKPatel-1996 <rrohit3rock@gmail.com>
2026-09-03 19:11:06 +0530

    Migrate RK-2026-004 operating systems article

--- src/content/manifest.ts
+++ src/content/manifest.ts
@@ -1,6 +1,27 @@
 // AUTO-GENERATED FILE. DO NOT EDIT DIRECTLY.
 export const ARTICLE_MANIFEST = [
   {
+    "id": "RK-2026-004",
+    "title": "Operating systems: Windows, vs Linux",
+    "date": "2026-07-07",
+    "tags": [
+      "#OperatingSystems",
+      "#Windows",
+      "#Linux"
+    ],
+    "excerpt": "\n\n",
+    "readTime": "30 min",
+    "type": "report",
+    "template": "standard",
+    "author": {
+      "name": "RK Patel",
+      "role": "Microbiologist",
+      "avatar": "https://github.com/RKPatel-1996.png",
+      "affiliation": "Gujarat University"
+    },
+    "modulePath": "./articles/rk-2026-004.ts"
+  },
+  {
     "id": "RK-2026-003",
     "title": "Computer Hardware: CPU, RAM, and Storage",
     "date": "2026-07-06",
